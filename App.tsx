f74fab6fd0cb9cf5e6f9013c0fc2768af799f607
adalid2608 <adalid.islas26@gmail.com>
2023-10-11 18:47:52 -0600

    Integración de Imagen de criptomonedas

--- App.tsx
+++ App.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Image} from 'react-native';
 import Header from './components/Header';
 
 const App = () => {
   return (
     <>
       <Header />
+      <Image
+        source={require('./assets/img/cryptomonedas.png')}
+        style={styles.imagen}
+      />
     </>
   );
 }
 
 const styles = StyleSheet.create({
-
+  imagen: {
+    width: '100%',
+    height: 150
+  }
 });
 
 export default App;
